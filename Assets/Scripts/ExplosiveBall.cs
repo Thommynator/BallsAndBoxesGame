55ec14cfefafa8679a810c2572913645a8679062
Thommynator <Thommynator@users.noreply.github.com>
2023-07-29 23:19:24 +0200

    Change explosion ball behavior. No hit damage

--- Assets/Scripts/ExplosiveBall.cs
+++ Assets/Scripts/ExplosiveBall.cs
@@ -5,23 +5,14 @@ public class ExplosiveBall : Ball
 
     protected override void ApplyDamageEffect(Collision2D collision)
     {
-        base.ApplyDamageEffect(collision);
-
         var contactPoint = collision.contacts[0].point;
         var surroundingBlocks = Physics2D.OverlapCircleAll(contactPoint, _stats.TryToGetStat(Stat.EXPLOSION_RANGE), _dealsDamageTo);
 
         foreach (var blockCollider in surroundingBlocks)
         {
-            if(blockCollider == collision.collider)
-            {
-                // this one was directly hit and doesn't receive an additional explosion damage
-                continue;
-            }
-
             blockCollider.TryGetComponent(out Block block);
             block?.DecreaseHealthBy((int)_stats.TryToGetStat(Stat.EXPLOSION_DAMAGE)); 
         }
-
     }
 
 
