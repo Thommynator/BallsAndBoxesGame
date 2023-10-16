a37fad8830191c5ca880ae140c63c027a6a2a620
Thomas Schindler <Thommynator@users.noreply.github.com>
2023-10-16 23:40:44 +0200

    Fix build issues (#22)

--- Assets/Scripts/StatsVisualizer.cs
+++ Assets/Scripts/StatsVisualizer.cs
@@ -2,10 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-<<<<<<< HEAD
-=======
-using static UnityEditor.Progress;
->>>>>>> master
 
 public class StatsVisualizer : MonoBehaviour
 {
@@ -20,16 +16,11 @@ public class StatsVisualizer : MonoBehaviour
     [SerializeField]
     private GameObject _content;
 
-<<<<<<< HEAD
     void Awake()
-=======
-    void Start()
->>>>>>> master
     {
         foreach (var item in _stats.GetStats())
         {
             Instantiate(_fieldPrefab, this.transform).Initialize(_stats, item.Key).transform.SetParent(_content.transform);
         }
     }
-
 }
